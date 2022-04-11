1da90c73d4e398d280185fe8caebed04ee829112
priya <pn713145@gmail.com>
2022-04-11 22:32:18 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,56 +1,68 @@
-
-import React from 'react'
-
-import './App.css';
+// import React from "react";
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <>
-    <div className="container">
+      <div className="container">
         <div className="main-content">
-          <p className="text">GeeksforGeeks</p>
-        </div>
+          <p className="text"> GeeksfrGeeks </p>{" "}
+        </div>{" "}
         <div className="centre-content">
-          <h1 className="price">pay your bill<span>/-</span></h1>
-          <p className="course">
-            Buy your favourite food Now !
-          </p>
-        </div>
+          <h1 className="price">
+            {" "}
+            pay your bill <span> /-</span>{" "}
+          </h1>{" "}
+          <p className="course">Buy your favourite food Now!</p>{" "}
+        </div>{" "}
         <div className="last-content">
           <button type="button" className="pay-now-btn">
-            Apply Coupons
-          </button>
+            Apply Coupons{" "}
+          </button>{" "}
           <button type="button" className="pay-now-btn">
-            Pay with Netbanking
-          </button>
+            Pay with Netbanking{" "}
+          </button>{" "}
           {/* <button type="button" class="pay-now-btn">
-		Netbanking options
-		</button> */}
-        </div>
+            		Netbanking options
+            		</button> */}{" "}
+        </div>{" "}
         <div className="card-details">
-          <p>Pay using Credit or Debit card</p>
+          <p> Pay using Credit or Debit card </p>{" "}
           <div className="card-number">
-            <label> Card Number </label>
-            <input type="text" className="card-number-field" placeholder="###-###-###" />
-          </div>
+            <label> Card Number </label>{" "}
+            <input
+              type="text"
+              className="card-number-field"
+              placeholder="###-###-###"
+            />
+          </div>{" "}
           <br />
           <div className="date-number">
-            <label> Expiry Date </label>
-            <input type="text" className="date-number-field" placeholder="DD-MM-YY" />
-          </div>
+            <label> Expiry Date </label>{" "}
+            <input
+              type="text"
+              className="date-number-field"
+              placeholder="DD-MM-YY"
+            />
+          </div>{" "}
           <div className="cvv-number">
-            <label> CVV number </label>
+            <label> CVV number </label>{" "}
             <input type="text" className="cvv-number-field" placeholder="xxx" />
-          </div>
+          </div>{" "}
           <div className="nameholder-number">
-            <label> Card Holder name </label>
-            <input type="text" className="card-name-field" placeholder="Enter your Name" />
-          </div>
+            <label> Card Holder name </label>{" "}
+            <input
+              type="text"
+              className="card-name-field"
+              placeholder="Enter your Name"
+            />
+          </div>{" "}
           <button type="submit" className="submit-now-btn">
-            submit
-          </button>
-        </div>
-      </div>
+            submit{" "}
+          </button>{" "}
+        </div>{" "}
+      </div>{" "}
     </>
   );
 }
